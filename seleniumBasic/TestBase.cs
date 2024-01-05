24d900e0affdd5bbfbac64e0de74f7fcdcb5bb9c
aaleksiuk <aal@scalepoint.com>
2024-01-05 15:52:07 +0100

    testBase refactorsmall refactor2

---
+++ seleniumBasic/TestBase.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumBasic
+{
+    public abstract class TestBase
+    {
+        protected readonly IWebDriver driver;
+        public TestBase()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("http://www.seleniumui.moderntester.pl/form.php");
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
