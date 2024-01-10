ee71149a5dee9254b864c6eb0339e7c90c93af70
aaleksiuk <aal@scalepoint.com>
2024-01-10 13:32:32 +0100

    Tooltip changes

---
+++ seleniumBasic/Widgets/Tooltip.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using SeleniumBasic;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace seleniumBasic.Widgets
+{
+    public class Tooltip : TestBase
+    {
+        public Tooltip(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void PrintOutMsgFromTooltip()
+        {
+            driver.Navigate().GoToUrl("http://www.seleniumui.moderntester.pl/tooltip.php");
+            IWebElement age = driver.FindElement(By.CssSelector("#age"));
+            var actualTooltip = age.GetAttribute("title");
+            output.WriteLine(actualTooltip);
+        }
+    }
+}
