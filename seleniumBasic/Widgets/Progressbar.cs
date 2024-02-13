2b1f61c7ac66c37e1dc3cb3ffb681cbed1d3cadf
aaleksiuk <aal@scalepoint.com>
2024-02-13 15:15:28 +0100

    progressbar handling

--- seleniumBasic/Widgets/Progressbar.cs
+++ seleniumBasic/Widgets/Progressbar.cs
@@ -1,11 +1,10 @@
-﻿using SeleniumBasic;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumBasic;
+using SeleniumExtras.WaitHelpers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit.Abstractions;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace seleniumBasic.Widgets
 {
@@ -16,8 +15,20 @@ namespace seleniumBasic.Widgets
         }
 
         [Fact]
-        public void ProgressbarV1() { }
-        [Fact]
-        public void ProgressbarV2() { }
+        public void ExpectedLabel()//Pierwsza wersja:Wait until the text in progress bar label will be "Complete!"
+        {
+            driver.Navigate().GoToUrl("http://www.seleniumui.moderntester.pl/progressbar.php");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            //wait.Until(d => d.FindElement(By.XPath("//div[@class='progress-label']")).GetAttribute("value") == "Complete!");
+            wait.Until(d => d.FindElement(By.XPath("//div[contains(text(),'Complete!')][@class='progress-label']")));
+            
+        }
+        [Fact] //Wait until ‘class’ attribute of progress bar will contain text: "ui-progressbar-complete"
+        public void ClassContainText()
+        {
+            driver.Navigate().GoToUrl("http://www.seleniumui.moderntester.pl/progressbar.php");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[contains(@class, 'ui-progressbar-complete')]")));
+        }
     }
 }
